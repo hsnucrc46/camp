cbbac60fa625ecc7ec693a174521ef25ff02d90e
Oliver Tzeng <olivertzeng@proton.me>
2024-06-13 06:48:40 +0800

    fix(settings.sty): make settings great again

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-for f in ./**/*.tex; do
-	filename=$(basename "$f")
-	echo "Now compiling $filename..."
-	cd "$(dirname "$f")" || return
-	if [ -f "$filename" ]; then
-		xelatex -synctex=1 -interaction=nonstopmode --shell-escape --enable-8bit-chars -recorder "$filename"
-	else
-		echo "Error: File not found: $filename"
-	fi
-done
+find . -type f -name "*.tex" -exec xelatex -interaction=nonstopmode -shell-escape -8bit -recorder {} \;
